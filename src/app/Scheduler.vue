723c397aa93d8551547bd33000cd8d1e3dfce22f
Heather Frantisak <hfrantisak@mvnp.com>
2017-04-03 16:23:38 -1000

    Add comments to Scheduler component

--- src/app/Scheduler.vue
+++ src/app/Scheduler.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="scheduler">
     <h1>{{ scheduler }}</h1>
+    <!-- Output all jobs in the array, as user adds more jobs -->
     <div v-for="job in jobsArray">
       Job: {{ job }}
     </div>
@@ -11,6 +12,8 @@
     <div v-else>
       <h2>Click to add job</h2>
     </div>
+    <!-- Toggle between showing input fields,
+    when true the input field section is shown -->
     <button v-on:click="showForm = !showForm">Add new job</button>
   </div>
 </template>
@@ -20,12 +23,15 @@ export default {
   name: 'Scheduler',
   data: {
     return {
+      // Store user input for added jobs
       jobsArray: [],
       scheduler: 'Heroku Scheduler Clone',
+      // Default to false, for hiding input field
       showForm: false,
     };
   },
   methods: {
+    // Add job from input field
     postJob: function(jobModel) {
       this.jobsArray.push(jobModel)
     }
