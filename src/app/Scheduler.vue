6685ef1df8307f4dd424a8b6f6d485b2d37ed1b5
Heather Frantisak <hfrantisak@mvnp.com>
2017-04-03 16:18:53 -1000

    Change data syntax for consistency. Remove unused attributes, other attrs unrelated to vuejs

--- src/app/Scheduler.vue
+++ src/app/Scheduler.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="scheduler">
     <h1>{{ scheduler }}</h1>
-    <div v-for="job in jobsArray" class="">
+    <div v-for="job in jobsArray">
       Job: {{ job }}
     </div>
     <div v-if="showForm">
-      <input v-model="jobModel" type="text" name="" value="">
-      <button v-on:click="postJob(jobModel)" type="button" name="button">Save</button>
+      <input v-model="jobModel" type="text">
+      <button v-on:click="postJob(jobModel)">Save</button>
     </div>
     <div v-else>
       <h2>Click to add job</h2>
     </div>
-    <button v-on:click="showForm = !showForm" type="button" name="button">Add new job</button>
+    <button v-on:click="showForm = !showForm">Add new job</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Scheduler',
-  data() {
+  data: {
     return {
       jobsArray: [],
       scheduler: 'Heroku Scheduler Clone',
